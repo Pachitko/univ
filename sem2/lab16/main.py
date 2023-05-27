f439d7d01fe0d11c12a385e6736adf185c01651f
pachitko <pachitko@mail.ru>
2023-05-27 18:01:09 +0300

    init lab 16

--- sem2/lab16/main.py
+++ sem2/lab16/main.py
@@ -1,3 +1,18 @@
 import PySimpleGUI as sg
+import os
+from os.path import isfile, join
 
-sg.Window(title="Hello World", layout=[[]], margins=(100, 50)).read()
+cwd = os.getcwd()
+
+layout = [[sg.Button(button_text="OK")], [sg.Text(cwd)], [sg.Listbox(os.listdir(cwd), size=(
+    40, 10), select_mode=sg.LISTBOX_SELECT_MODE_MULTIPLE, key='lb', enable_events=True)]]
+
+window = sg.Window("File Explorer", layout)
+lb = window['lb']
+
+while True:
+    event, values = window.read()
+    if event == sg.WIN_CLOSED:
+        break
+
+window.close()
